5b8b75cba267e0ee05ffaaf597e326efc3b02af6
GAJENDRA SINGH <31224456+gajendragajay@users.noreply.github.com>
2018-10-30 02:36:45 +0530

    Add files via upload

---
+++ FileMerger/src/filemerger/FileMerger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filemerger;
+
+/**
+ *
+ * @author Gajendra
+ */
+public class FileMerger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new main().setVisible(true);
+    }
+    
+}
